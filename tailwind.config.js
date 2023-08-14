c3c4db781b4a3c59a916ea27a371287b9fb5476e
Fatih Kilic <me@fklc.dev>
2023-08-13 20:11:39 -0400

    Modified single-event.html to support a mock API

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./**/*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
 
